79041a86e5ccc59c8bb042c0fdcf6f65c55c4a47
Bondi <kirkizon5@gmail.com>
2022-06-09 12:20:34 +0300

    adaptive

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -8,9 +8,29 @@ import { useTypedSelector } from './../../utils/hooks/useTypedSelector';
 
 const Header: FC = () => {
 	const { totalPrice, productsCount } = useTypedSelector(state => state.cartReducer)
+	const [menuActive, setMenuActive] = React.useState(false);
 	return (
 		<header className={st.header}>
-			<div className={cn(st.container, st.grid)}>
+			<div className={cn(st.adaptiveLines)}>
+				<div onClick={() => setMenuActive(!menuActive)} className={cn(st.lines, { [st.linesAnimation]: menuActive })}>
+					<span></span>
+					<span></span>
+				</div>
+
+				<div className={cn(st.storeName)}>
+					<img src={logo} alt="pizza" />
+					<div className={cn(st.name)}>PIZZA STORE</div>
+				</div>
+				<NavLink onClick={() => setMenuActive(false)} to='/cart' className={cn(st.adaptiveCart)}>
+					<div className={cn(st.cartLogo)}>
+						<img src={cartLogo} alt="cart" />
+						<div className={st.productsCount}>{productsCount}</div>
+					</div>
+				</NavLink>
+
+			</div>
+			<div className={cn(st.container, st.grid, { [st.containerActive]: menuActive })}>
+
 				<div className={cn(st.storeName)}>
 					<img src={logo} alt="pizza" />
 					<div className={cn(st.name)}>PIZZA STORE</div>
