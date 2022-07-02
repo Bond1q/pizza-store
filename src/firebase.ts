3499a100ca12bca5c567004fab1543df047d8ee5
Bondi <kirkizon5@gmail.com>
2022-07-02 11:12:02 +0300

    env file

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,19 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: "AIzaSyBkDEc9VcKNH8te8aorERW92uCOajtwOAA",
-	authDomain: "pizza-store-31ba4.firebaseapp.com",
-	projectId: "pizza-store-31ba4",
-	storageBucket: "pizza-store-31ba4.appspot.com",
-	messagingSenderId: "779605830587",
-	appId: "1:779605830587:web:758b70ce5962ae0d17329e"
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore()
