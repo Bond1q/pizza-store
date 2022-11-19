5cbc61799d133022d3534adf4a88eed3bc00a056
Bondi <kirkizon5@gmail.com>
2022-11-20 00:42:35 +0200

    format

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -9,87 +9,63 @@ import cartLogo from '../../assets/imgs/carLogo.png'
 import st from '../Header/Header.module.scss'
 
 const Header: FC = () => {
-	const { totalPrice, productsCount } = useAppSelector((state) => state.cart)
-	const { getCartProducts } = useAppDispatch()
-	const [menuActive, setMenuActive] = React.useState(false)
-	const hideMenu = () => {
-		if (menuActive) {
-			setMenuActive(false)
-		}
-	}
-	React.useEffect(() => {
-		getCartProducts()
-	}, [])
-	return (
-		<header className={st.header}>
-			{/* <div className={cn(st.adaptiveLines)}>
-				<div
-					onClick={() => setMenuActive(!menuActive)}
-					className={cn(st.lines, { [st.linesAnimation]: menuActive })}
-				>
-					<span></span>
-					<span></span>
-				</div>
+   const { totalPrice, productsCount } = useAppSelector((state) => state.cart)
+   const { getCartProducts } = useAppDispatch()
+   const [menuActive, setMenuActive] = React.useState(false)
+   const hideMenu = () => {
+      if (menuActive) {
+         setMenuActive(false)
+      }
+   }
+   React.useEffect(() => {
+      getCartProducts()
+   }, [])
 
-				<div className={cn(st.storeName)}>
-					<img src={logo} alt='pizza' />
-					<div className={cn(st.name)}>PIZZA STORE</div>
-				</div>
-				<NavLink
-					onClick={() => setMenuActive(false)}
-					to='/cart'
-					className={cn(st.adaptiveCart)}
-				>
-					<div className={cn(st.cartLogo)}>
-						<img src={cartLogo} alt='cart' />
-						<div className={st.productsCount}>{productsCount}</div>
-					</div>
-				</NavLink>
-			</div> */}
+   return (
+      <header className={st.header}>
+         <div
+            onClick={hideMenu}
+            className={cn(st.container, st.grid, { [st.containerActive]: menuActive })}
+         >
+            <div
+               onClick={() => setMenuActive(!menuActive)}
+               className={cn(st.lines, { [st.linesAnimation]: menuActive })}
+            >
+               <span></span>
+               <span></span>
+            </div>
+            <div className={cn(st.storeName)}>
+               <img src={logo} alt='pizza' />
+               <div className={cn(st.name)}>PIZZA STORE</div>
+            </div>
+            <div className={cn(st.menu)}>
+               <NavLink
+                  style={({ isActive }) => ({ fontWeight: isActive ? 500 : 400 })}
+                  to='/pizzas'
+               >
+                  Pizzas
+               </NavLink>
+               <NavLink
+                  style={({ isActive }) => ({ fontWeight: isActive ? 500 : 400 })}
+                  to='/drinks'
+               >
+                  Drinks
+               </NavLink>
+            </div>
+            <NavLink to='/cart' className={cn(st.cart)}>
+               <div className={st.flexContainer}>
+                  <div className={cn(st.cost)}>{totalPrice} $</div>
+                  <span></span>
 
-			<div
-				onClick={hideMenu}
-				className={cn(st.container, st.grid, { [st.containerActive]: menuActive })}
-			>
-				<div
-					onClick={() => setMenuActive(!menuActive)}
-					className={cn(st.lines, { [st.linesAnimation]: menuActive })}
-				>
-					<span></span>
-					<span></span>
-				</div>
-				<div className={cn(st.storeName)}>
-					<img src={logo} alt='pizza' />
-					<div className={cn(st.name)}>PIZZA STORE</div>
-				</div>
-				<div className={cn(st.menu)}>
-					<NavLink
-						style={({ isActive }) => ({ fontWeight: isActive ? 500 : 400 })}
-						to='/pizzas'
-					>
-						Pizzas
-					</NavLink>
-					<NavLink
-						style={({ isActive }) => ({ fontWeight: isActive ? 500 : 400 })}
-						to='/drinks'
-					>
-						Drinks
-					</NavLink>
-				</div>
-				<NavLink to='/cart' className={cn(st.cart)}>
-					<div className={st.flexContainer}>
-						<div className={cn(st.cost)}>{totalPrice} $</div>
-						<span></span>
-
-						<div className={cn(st.cartLogo)}>
-							<img src={cartLogo} alt='cart' />
-							<div className={st.productsCount}>{productsCount}</div>
-						</div>
-					</div>
-				</NavLink>
-			</div>
-		</header >
-	)
+                  <div className={cn(st.cartLogo)}>
+                     <img src={cartLogo} alt='cart' />
+                     <div className={st.productsCount}>{productsCount}</div>
+                  </div>
+               </div>
+            </NavLink>
+         </div>
+      </header>
+   )
 }
 
 export default Header
